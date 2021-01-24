7beee120b933733ae4e1d7b8717f94360613c2d3
Dabdibow <64368085+Dabdibow@users.noreply.github.com>
2021-01-24 17:08:52 +0300

    Delete web_application directory

--- web_application/src/main/java/ru/itis/javalab/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.javalab.repositories;
-
-import ru.itis.javalab.models.Student;
-
-import java.util.List;
-
-public interface StudentRepository extends CrudRepository<Student> {
-    List<Student> findAll();
-}
--- web_application/src/main/java/ru/itis/javalab/services/StudentService.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.javalab.services;
-
-import ru.itis.javalab.models.Student;
-
-import java.util.List;
-
-public interface StudentService {
-    List<Student> findAll();
-}
